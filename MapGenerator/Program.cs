1ce6faa452e981a58498b30e2cd8e494ab82da8d
Colin Davis <cdavis@enviroissues.com>
2013-12-30 18:57:34 -0800

    first commit, trying to get layering working

---
+++ MapGenerator/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Linq;
+
+namespace MapGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test();
+            SaveNoiseImage();
+        }
+
+        private static void Test()
+        {
+            List<int> nums = new List<int>();
+            var rand = new Random();
+            for (var i = 0; i < 1000000; i++)
+            {
+                var noise = new Noise(128, 128);
+                nums.Add((int) noise.Generate(rand.Next(), rand.Next(), 8.0));
+            }
+            Console.WriteLine(nums.Min());
+            Console.WriteLine(nums.Max());
+            Console.WriteLine(nums.Average());
+            Console.ReadLine();
+        }
+
+        private static void SaveNoiseImage()
+        {
+            var image = new Bitmap(1024, 768);
+
+            for (var i = 0; i < image.Width; i++)
+            {
+                for (var j = 0; j < image.Height; j++)
+                {
+                    var noiseGen = new Noise(128, 128);
+
+                    var colorInt = (int)noiseGen.Generate(i, j, 8.0);
+                    var color = Color.FromArgb(255, colorInt, colorInt, colorInt);
+                    image.SetPixel(i, j, color);
+                }
+            }
+
+            image.Save(@"C:\tmp\perlin" + ".jpg", ImageFormat.Jpeg);
+        }
+    }
+}
---
+++ UnitTests/NoiseTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MapGenerator;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class NoiseTests
+    {
+        [Test]
+        public void ItShouldReturnBetweenZeroAndOne()
+        {
+            var rand = new Random();
+
+            for (var i = 0; i < 100; i++)
+            {
+                var result = SimplexNoise.Generate2D(NextFloat(rand), NextFloat(rand));
+                Assert.GreaterOrEqual(result, 0.0f);
+                Assert.LessOrEqual(result, 1.0f);
+            }
+        }
+
+        private static float NextFloat(Random random)
+        {
+            double mantissa = (random.NextDouble()*2.0) - 1.0;
+            double exponent = Math.Pow(2.0, random.Next(-126, 128));
+            return (float) (mantissa*exponent);
+        }
+    }
+}
